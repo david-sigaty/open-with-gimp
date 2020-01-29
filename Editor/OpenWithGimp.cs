0abfb3262fcf2bc29c1aa4f6885caf2de4d816b8
david-sigaty <david@sigaty.dev>
2020-01-29 09:48:18 -0700

    Initial commit.

---
+++ Editor/OpenWithGimp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using TeamCitrus.Editor;
+
+public class OpenWithGimp : OpenWithMenuItem
+{
+	public const string Name = "GIMP";
+	public const string Key = BaseKey + Name;
+	private const string MenuItemPath = BasePath + Name;
+	
+	[MenuItem(MenuItemPath, false, BasePriority + 51)]
+	public static void Open() { Execute<OpenWithGimp>(); }
+
+	[MenuItem(MenuItemPath, true)]
+	public static bool OpenValidation() { return Validate<OpenWithGimp>(); }
+}
